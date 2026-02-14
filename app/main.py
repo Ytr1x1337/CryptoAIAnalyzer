e9917424370f7c23037198e78f9d8cd41efd758d
Ytr1x1337 <ytr1x1447@gmail.com>
2026-02-14 10:31:45 +0300

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from routers import router
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI(title="Crypto AI Analyser")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+if __name__ == "__main__":
+
+    uvicorn.run("main:app", reload=True)
